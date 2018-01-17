ce059923354e992672df4a34f046a390f6127af0
Ronny Kemmereit <office@manetage.de>
2018-01-17 13:11:37 +0100

    - bugfix: css instead js

--- plugins/autocomplete/pages/index.php
+++ plugins/autocomplete/pages/index.php
@@ -123,12 +123,12 @@ $content = $fragment->parse('core/page/section.php');
 echo $content;
 
 
-$code = '<link rel="stylesheet" type="text/css" href="/'. substr(rex_url::pluginAssets('search_it', 'autocomplete','jquery.suggest.js'),3).'" media="screen" />
+$code = '<link rel="stylesheet" type="text/css" href="/'. substr(rex_url::pluginAssets('search_it', 'autocomplete','jquery.suggest.css'),3).'" media="screen" />
 <script type="text/javascript" src="/'. substr(rex_url::pluginAssets('search_it', 'autocomplete','jquery.suggest.js'),3).'"></script>          
 <script type="text/javascript">
   jQuery(document).ready(function() {
     jQuery(function() {
-      jQuery(".search_it-form input[name=search]").suggest("index.php?rex-api-call=autocompleteHandler&rnd=" + Math.random()###AUTOSUBMIT###);      
+      jQuery(".search_it-form input[name=search]").suggest("index.php?rex-api-call=search_it_autocomplete_getSimilarWords&rnd=" + Math.random()###AUTOSUBMIT###);      
     });
   });  
 </script>';

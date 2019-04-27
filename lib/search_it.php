00e275d808589b91d3b262ccf3f5d5e63dec1624
Markus Staab <maggus.staab@googlemail.com>
2019-04-27 20:55:11 +0200

    Fixed „non-numeric value encountered“ warning

--- lib/search_it.php
+++ lib/search_it.php
@@ -129,10 +129,10 @@ class search_it
 
 
         // index articles
-        $global_return = '';
+        $global_return = 0;
         $art_sql = rex_sql::factory();
         $art_sql->setTable($this->tablePrefix . 'article');
-        if ($art_sql->select('id,clang_id')) {
+        if ($art_sql->select('id,clang_id')) 
             foreach ($art_sql->getArray() as $art) {
                 $returns = $this->indexArticle($art['id'], $art['clang_id']);
                 foreach ( $returns as $return ) {

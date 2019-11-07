41ea8595cfdef3e253dbd99bbdb5281b1ee64b62
Norbert Micheel <nm@lautundschoen.de>
2019-11-07 01:16:32 +0100

    falsche Übersetzungen entfernt, install.sql ->install.php

---
+++ install.php
@@ -0,0 +1,49 @@
+<?php
+
+rex_sql_table::get(rex::getTable('search_it_index'))
+    ->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
+    ->ensureColumn(new rex_sql_column('fid', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('catid', 'int(11)', true))
+    ->ensureColumn(new rex_sql_column('ftable', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('fcolumn', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('texttype', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('clang', 'int(11)', true))
+    ->ensureColumn(new rex_sql_column('filename', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('fileext', 'varchar(255)', true))
+    ->ensureColumn(new rex_sql_column('plaintext', 'longtext'))
+    ->ensureColumn(new rex_sql_column('unchangedtext', 'longtext'))
+    ->ensureColumn(new rex_sql_column('teaser', 'longtext'))
+    ->ensureColumn(new rex_sql_column('values', 'longtext'))
+    ->setPrimaryKey('id')
+    ->ensureIndex(new rex_sql_index('fid', ['fid']))
+    ->ensureIndex(new rex_sql_index('plaintext', ['plaintext'], rex_sql_index::FULLTEXT))
+    ->ensureIndex(new rex_sql_index('unchangedtext', ['unchangedtext'], rex_sql_index::FULLTEXT))
+    ->ensureIndex(new rex_sql_index('plaintext_2', ['plaintext', 'unchangedtext'], rex_sql_index::FULLTEXT))
+    ->ensure();
+
+
+rex_sql_table::get(rex::getTable('search_it_cache'))
+    ->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
+    ->ensureColumn(new rex_sql_column('hash', 'char(32)'))
+    ->ensureColumn(new rex_sql_column('returnarray', 'longtext', true))
+    ->setPrimaryKey('id')
+    ->ensure();
+
+rex_sql_table::get(rex::getTable('search_it_cacheindex_ids'))
+    ->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
+    ->ensureColumn(new rex_sql_column('index_id', 'int(11)', true))
+    ->ensureColumn(new rex_sql_column('cache_id', 'varchar(255)', true))
+    ->setPrimaryKey('id')
+    ->ensure();
+
+rex_sql_table::get(rex::getTable('search_it_keywords'))
+    ->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
+    ->ensureColumn(new rex_sql_column('keyword', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('soundex', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('metaphone', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('colognephone', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('clang', 'int(11)', false, '-1'))
+    ->ensureColumn(new rex_sql_column('count', 'int(11)', false, '1'))
+    ->setPrimaryKey('id')
+    ->ensureIndex(new rex_sql_index('keyword', ['keyword', 'clang'], rex_sql_index::UNIQUE))
+    ->ensure();
--- plugins/stats/install.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS `%TABLE_PREFIX%search_it_stats_searchterms` (
-  `id` int(11) NOT NULL auto_increment,
-  `term` varchar(255) NOT NULL,
-  `time` datetime NOT NULL,
-  `resultcount` int(11) NOT NULL DEFAULT 0,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM;

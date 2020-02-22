1cffa39238eb22492e2a1417efd76d42aedd0763
Alexander Walther <github.com@alexplus.de>
2020-02-22 01:02:56 +0100

    tmp-Präfix an db-Tabellen
    closes #226

--- uninstall.sql
+++ uninstall.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS `%TABLE_PREFIX%search_it_index`;
-DROP TABLE IF EXISTS `%TABLE_PREFIX%search_it_cache`;
-DROP TABLE IF EXISTS `%TABLE_PREFIX%search_it_cacheindex_ids`;
-DROP TABLE IF EXISTS `%TABLE_PREFIX%search_it_keywords`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%tmp_search_it_index`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%tmp_search_it_cache`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%tmp_search_it_cacheindex_ids`;
+DROP TABLE IF EXISTS `%TABLE_PREFIX%tmp_search_it_keywords`;
--- update.php
+++ update.php
@@ -14,3 +14,13 @@ if ( is_dir($this->getPlugin('search_highlighter')->getPath()) ) {
     //echo rex_view::warning($this->i18n('search_it_settings_plugin_deleted'));
 }
 
+$result = rex_sql::factory()->getArray("SELECT count(*) as `count`
+FROM information_schema.TABLES
+WHERE (TABLE_NAME = 'rex_search_it_index')");
+
+if($result[0]['count'] > 0) {
+    rex_sql::factory()->setQuery("RENAME TABLE rex_search_it_cache TO rex_tmp_search_it_cache");
+    rex_sql::factory()->setQuery("RENAME TABLE rex_search_it_cacheindex_ids TO rex_tmp_search_it_cacheindex_ids");
+    rex_sql::factory()->setQuery("RENAME TABLE rex_search_it_index TO rex_tmp_search_it_index");
+    rex_sql::factory()->setQuery("RENAME TABLE rex_search_it_keywords TO rex_tmp_search_it_keywords");
+}

087a7bac40bb06f43d3906644da6bd900de607e3
Alexander Walther <github.com@alexplus.de>
2020-04-20 14:19:39 +0200

    unbeliebte Tabellen verstecken (Entwurf)

--- pages/settings.addtables.php
+++ pages/settings.addtables.php
@@ -1,12 +1,24 @@
 <?php
 
+$hidden_tables = ["rex_action","rex_clang","rex_config","rex_media_manager_type","rex_media_manager_type_effect","rex_metainfo_field","rex_metainfo_type","rex_user","rex_user_role","rex_ydeploy_migration","rex_yform_email_template","rex_yform_field","rex_yform_history","rex_yform_history_field","rex_yform_table","rex_yrewrite_alias","rex_yrewrite_domain","rex_yrewrite_forward","rex_article_slice_history","rex_cronjob","rex_url_generate","rex_yform_history","rex_yform_history_field","rex_yrewrite_alias","rex_yrewrite_domain","rex_yrewrite_forward"];
+
+rex_config::set("search_it", "hidden_tables", $hidden_tables);
+
 $addon = rex_addon::get('search_it');
 $form = rex_config_form::factory($addon->name);
 
 $form->addFieldset($this->i18n('search_it_settings'));
 
+
+
 $sql_tables = rex_sql::factory();
 foreach ($sql_tables->getTablesAndViews() as $table) {
+    $hidden_tables = rex_config::get("search_it", "hidden_tables");
+
+    if (in_array($table, $hidden_tables)) {
+        continue;
+    };
+
     $field = $form->addSelectField('similarwordsmode', $value = null, ['class'=>'form-control selectpicker']);
     $field->setAttribute('multiple', 'multiple');
     $field->setLabel($table);

9cd02678bbcf0936624dfca68fa6b7c5e8c1909d
Alexander Walther <github.com@alexplus.de>
2020-04-20 13:15:48 +0200

    move modi settings to result settings

--- pages/settings.result.php
+++ pages/settings.result.php
@@ -3,6 +3,41 @@
 $addon = rex_addon::get('search_it');
 $form = rex_config_form::factory($addon->name);
 
+$form->addFieldset($this->i18n('search_it_settings_modi_header'));
+
+$field = $form->addSelectField('logicalmode', $value = null, ['class'=>'form-control selectpicker']);
+$field->setLabel($this->i18n('search_it_settings_logicalmode'));
+$select = $field->getSelect();
+$select->addOption($this->i18n('search_it_settings_logicalmode_and'), 'and');
+$select->addOption($this->i18n('search_it_settings_logicalmode_or'), 'or');
+
+$field = $form->addSelectField('textmode', $value = null, ['class'=>'form-control selectpicker']);
+$field->setLabel($this->i18n('search_it_settings_textmode'));
+$select = $field->getSelect();
+$select->addOption($this->i18n('search_it_settings_textmode_plain'), 'plain');
+$select->addOption($this->i18n('search_it_settings_textmode_html'), 'html');
+$select->addOption($this->i18n('search_it_settings_textmode_both'), 'both');
+
+$field = $form->addSelectField('similarwordsmode', $value = null, ['class'=>'form-control selectpicker']);
+$field->setLabel($this->i18n('search_it_settings_similarwords_label'));
+$select = $field->getSelect();
+$select->addOption($this->i18n('search_it_settings_similarwords_none'), SEARCH_IT_SIMILARWORDS_NONE);
+$select->addOption($this->i18n('search_it_settings_similarwords_soundex'), SEARCH_IT_SIMILARWORDS_SOUNDEX);
+$select->addOption($this->i18n('search_it_settings_similarwords_metaphone'), SEARCH_IT_SIMILARWORDS_METAPHONE);
+$select->addOption($this->i18n('search_it_settings_similarwords_cologne'), SEARCH_IT_SIMILARWORDS_COLOGNEPHONE);
+$select->addOption($this->i18n('search_it_settings_similarwords_all'), SEARCH_IT_SIMILARWORDS_ALL);
+
+$field = $form->addCheckboxField('similarwords_permanent');
+$field->setLabel($this->i18n('search_it_settings_similarwords_permanent'));
+$field->addOption($this->i18n('search_it_settings_similarwords_permanent'), '1');
+
+$field = $form->addSelectField('searchmode', $value = null, ['class'=>'form-control selectpicker']);
+$field->setLabel($this->i18n('search_it_settings_searchmode'));
+$select = $field->getSelect();
+$select->addOption($this->i18n('search_it_settings_searchmode_like'), 'like');
+$select->addOption($this->i18n('search_it_settings_searchmode_match'), 'match');
+$select->addOption('Vegetarisch', 'vegetarisch');
+
 $form->addFieldset($this->i18n('search_it_settings_search_highlighter'));
 
 $field = $form->addTextField('text', '', ["class" => "form-control"]);

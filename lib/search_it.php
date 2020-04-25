99671f719292c6eb4a128969a4bee3bac4d8cdc6
Tobias Krais <tk@design-to-use.de>
2020-04-25 17:38:25 +0200

    reindex updated urls

--- lib/search_it.php
+++ lib/search_it.php
@@ -536,9 +536,12 @@ class search_it
 
     /**
      * Compares index table with url addon table and adds new urls to index
+	 * @return string[] Array containing index results
      */
     public function indexNewURLs()
     {
+        $global_return = 0;
+
 		// index url 2 addon URLs
 		if(rex_addon::get('search_it')->getConfig('index_url_addon') && search_it_isUrlAddOnAvailable()) {
 			$sql = rex_sql::factory();
@@ -547,6 +550,32 @@ class search_it
 				. "WHERE search_it.fid IS NULL;");
 
 			foreach ($sql->getArray() as $result) {
+				$returns = $this->indexUrl($result['url_hash'], $result['article_id'], $result['clang_id'], $result['profile_id'], $result['data_id']);
+				foreach ( $returns as $return ) {
+					if ($return > 3 ) { $global_return += $return; }
+				}
+			}
+		}
+		return $global_return;
+    }
+
+    /**
+     * Compares index table with url addon table and adds new urls to index
+	 * @return string[] Array containing index results
+     */
+    public function indexUpdatedURLs()
+    {
+        $global_return = 0;
+
+		// index url 2 addon URLs
+		if(rex_addon::get('search_it')->getConfig('index_url_addon') && search_it_isUrlAddOnAvailable()) {
+			$sql = rex_sql::factory();
+			$sql->setQuery("SELECT url.url_hash, url.article_id, url.clang_id, url.profile_id, url.data_id FROM `". search_it_getUrlAddOnTableName() ."` as url "
+				. "LEFT JOIN `". $this->tempTablePrefix ."search_it_index` AS search_it ON url.url_hash = search_it.fid "
+				. "WHERE search_it.lastindexed < url.lastmod;");
+
+			foreach ($sql->getArray() as $result) {
+				$this->unindexURL($result['url_hash']);
 				$returns = $this->indexUrl($result['url_hash'], $result['article_id'], $result['clang_id'], $result['profile_id'], $result['data_id']);
 				foreach ( $returns as $return ) {
 					if ($return > 3 ) { $global_return += $return; }

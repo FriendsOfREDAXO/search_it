5e6ead454b5b7d678cdbf4a38dcb18953e8ba231
alexplusde <github.com@alexplus.de>
2020-05-06 14:48:26 +0200

    Suchbegriff hervorheben

--- fragments/search_it-result-no-result.php
+++ fragments/search_it-result-no-result.php
@@ -1,2 +1,2 @@
-<p class="search_it-no_result">Ihre Suche nach <?= $this->searchterm ?> ergab keine Treffer.
+<p class="search_it-no_result">Ihre Suche nach <mark><?= $this->searchterm ?></mark> ergab keine Treffer.
 </p>

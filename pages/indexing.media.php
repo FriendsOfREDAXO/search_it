c0e105ba8d34d5923f31719481e33168094808d0
alexplusde <github.com@alexplus.de>
2020-05-06 16:18:49 +0200

    update pages

--- pages/indexing.media.php
+++ pages/indexing.media.php
@@ -8,12 +8,16 @@ $field = $form->addCheckboxField('indexmediapool');
 $field->setLabel("");
 $field->addOption($this->i18n('search_it_settings_file_mediapool'), '1');
 
+$field = $form->addCheckboxField('indexmediapool_meta');
+$field->setLabel("");
+$field->addOption($this->i18n('search_it_settings_file_mediapool_meta'), '1');
+
 $field = $form->addTextField('media_extensions');
 $field->setLabel($this->i18n('search_it_settings_media_fileext_label'));
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', $addon->i18n('search_it_settings'), false);
+$fragment->setVar('title', $addon->i18n('search_it_indexing_mediapool'), false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
 
--- pages/indexing.url.php
+++ pages/indexing.url.php
@@ -3,15 +3,14 @@
 $addon = rex_addon::get('search_it');
 $form = rex_config_form::factory($addon->name);
 
-$form->addFieldset($this->i18n('search_it_settings_index_url_addon_label'));
-
 $field = $form->addCheckboxField('index_url_addon');
 $field->setLabel("");
+$field->setNotice($this->i18n('search_it_settings_index_url_addon_notice'));
 $field->addOption($this->i18n('search_it_settings_index_url_addon_label'), '1');
 
 $fragment = new rex_fragment();
 $fragment->setVar('class', 'edit', false);
-$fragment->setVar('title', $addon->i18n('search_it_settings'), false);
+$fragment->setVar('title', $addon->i18n('search_it_indexing_urls'), false);
 $fragment->setVar('body', $form->get(), false);
 echo $fragment->parse('core/page/section.php');
 

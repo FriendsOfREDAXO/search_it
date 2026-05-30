ffdb6d44762dd07fd8d29c000df52af75363e2bc
Thomas Skerbis <thomas.skerbis@mac.com>
2026-05-30 21:30:21 +0200

    API-Endpunkte fuer aktuelles search_it neu aufsetzen

---
+++ lib/Api/RoutePackage/Backend/SearchIt.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace FriendsOfRedaxo\SearchIt\Api\RoutePackage\Backend;
+
+use FriendsOfRedaxo\Api\Auth\BackendUser;
+use FriendsOfRedaxo\Api\RouteCollection;
+use FriendsOfRedaxo\SearchIt\Api\RoutePackage\SearchIt as TokenSearchIt;
+
+use function strlen;
+
+class SearchIt extends TokenSearchIt
+{
+    public function loadRoutes()
+    {
+        $routes = RouteCollection::getRoutes();
+
+        foreach ($routes as $route) {
+            if ('search_it/' === substr($route['scope'], 0, strlen('search_it/'))) {
+                $scope = 'backend/' . $route['scope'];
+                $newRoute = clone $route['route'];
+                $newRoute->setPath('backend/' . ltrim($newRoute->getPath(), '/'));
+
+                RouteCollection::registerRoute(
+                    $scope,
+                    $newRoute,
+                    $route['description'],
+                    $route['responses'],
+                    new BackendUser(),
+                    ['backend'],
+                );
+            }
+        }
+    }
+}
